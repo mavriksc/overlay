b68121ad055de8320f8383f12797264a8202eb29
Sean Carlisle <s.m.carlisle@gmail.com>
2026-01-02 15:57:46 -0600

    was initing with null bounds, using full screen of main monitor for now will be able to pull from game config

--- src/main/kotlin/org/mavriksc/overlay/GameOverlay.kt
+++ src/main/kotlin/org/mavriksc/overlay/GameOverlay.kt
@@ -3,6 +3,9 @@ package org.mavriksc.overlay
 import java.awt.*
 import java.awt.geom.Ellipse2D
 import javax.swing.JFrame
+import javax.swing.Timer
+import kotlin.Pair
+import kotlin.random.Random
 
 
 //this is for my ui. will need to get scale of hud and map from client and resolution in the future
@@ -14,45 +17,98 @@ data class OverlayConfig(
     var enableMapLookTimer: Boolean = true,
     var enableDodgeDirection: Boolean = true,
     var mapRect: Rectangle,
+    var mapFlashColor: Color = Color.WHITE,
     var northColor: Color = Color.RED,
     var southColor: Color = Color.BLUE,
     var eastColor: Color = Color.GREEN,
-    var westColor: Color = Color.YELLOW
+    var westColor: Color = Color.YELLOW,
+    var dodgeTimer: Int = 1_000,
+    var mapTimer: Int = 5_000,
+    var mapFlashTime: Int = 250
 )
-class GameOverlay: JFrame() {
+
+class GameOverlay : JFrame() {
+    private val fullScreenBounds = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.defaultConfiguration.bounds
+    var config = OverlayConfig(
+        mapRect = Rectangle(fullScreenBounds.width - 370, fullScreenBounds.height - 370, 350, 350),
+        northColor = Color.GREEN,
+        southColor = Color.GREEN,
+        eastColor = Color.GREEN,
+        westColor = Color.GREEN,
+        mapFlashColor = Color.WHITE,
+        dodgeTimer = 1_000,
+        mapTimer = 5_000
+    )
+    private var dodgeDir = Random.nextBoolean()
+    private var flashMap = false
+    private val p1 = Pair(20, 20)
+    private val p2 = Pair(fullScreenBounds.width - 20, 20)
+    private val p3 = Pair(fullScreenBounds.width - 20, fullScreenBounds.height - 20)
+    private val p4 = Pair(20, fullScreenBounds.height - 20)
+
     init {
         isUndecorated = true
         isAlwaysOnTop = true
         background = Color(0, 0, 0, 0)
         focusableWindowState = false
+        //start timers
+        Timer(config.dodgeTimer) {
+            dodgeDir = Random.nextBoolean()
+        }.start()
+
+        Timer(config.mapTimer) {
+            flashMap = true
+            repaint()
+            Timer(config.mapFlashTime) {
+                flashMap = false
+                repaint()
+            }.apply { isRepeats = false }.start()
+        }.start()
     }
 
     fun updateWindowBounds(b: Rectangle) {
         bounds = b
+
     }
 
     override fun paint(g: Graphics) {
         super.paint(g)
-        val g2d = g as Graphics2D
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g2d.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f) // 50% transparent
-        g2d.color = Color.RED
-        g2d.fill(Ellipse2D.Double(50.0, 50.0, 100.0, 100.0))
-        g2d.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f) // Fully opaque
-        g2d.color = Color.WHITE
-        g2d.font = Font("Arial", Font.BOLD, 24)
-        g2d.drawString("My Java Overlay!", 200, 100)
-        // draw a blue line 1 alpha 20 px in from bounds
-        val offset = 20
-        g2d.color = Color.BLUE
+        drawUI(g as Graphics2D)
+    }
+
+    private fun drawUI(g2d: Graphics2D) {
+        if (config.enableMapLookTimer && flashMap) drawMapLook(g2d)
+        if (config.enableSpellPacing) drawSpellPacing(g2d)
+        if (config.enableDodgeDirection) drawDodgeDirection(g2d)
+    }
+
+    private fun drawMapLook(g2d: Graphics2D) {
+        g2d.color = config.mapFlashColor
+        g2d.drawRect(config.mapRect.x, config.mapRect.y, config.mapRect.width, config.mapRect.height)
+        g2d.fillRect(config.mapRect.x, config.mapRect.y, config.mapRect.width, config.mapRect.height)
+    }
+
+    private fun drawDodgeDirection(g2d: Graphics2D) {
+        //True is up or left, and false is down or right
         g2d.stroke = BasicStroke(2f)
-        g2d.drawLine(offset, offset, bounds.width - offset, 20)
-        g2d.drawLine(bounds.width - offset, 20, bounds.width - offset, bounds.height - offset)
-        g2d.drawLine(bounds.width - offset, bounds.height - offset, offset, bounds.height - offset)
-        g2d.drawLine(offset, bounds.height - offset, offset, offset)
+        if (dodgeDir) {
+            //draw top
+            g2d.color = config.northColor
+            g2d.drawLine(p1.first, p1.second, p2.first, p2.second)
+
+            //draw left
+            g2d.color = config.westColor
+            g2d.drawLine(p1.first, p1.second, p4.first, p4.second)
+        } else {
+            //draw bottom
+            g2d.color = config.southColor
+            g2d.drawLine(p4.first, p4.second, p3.first, p3.second)
+
+            //draw right
+            g2d.color = config.eastColor
+            g2d.drawLine(p2.first, p2.second, p3.first, p3.second)
+        }
     }
 
-    fun drawUI(g2d: Graphics2D) {
-
-    }
+    private fun drawSpellPacing(g2d: Graphics2D) {}
 }
